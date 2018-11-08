2b1b2e16b901c5ff151a90269e1b4df7f383083f
Manuel Soler <manusoler@gmail.com>
2018-11-08 22:05:01 +0100

    New folders to set up the code

--- express/app.js
+++ express/app.js
@@ -2,12 +2,12 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
   console.log('In the middleware!');
-  next();
+  res.send('<h1>Add product page</h1>');
 });
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
   console.log('In another middleware!');
   res.send('<h1>Hi from Express.js</h1>');
 });
